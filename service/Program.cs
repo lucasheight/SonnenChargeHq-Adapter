c07d488d7237aa22e6e2168274821ea7c9ad3aaa
lucasheight <lyndengarrett@gmail.com>
2022-12-28 20:43:46 +1100

    cleanup

--- service/Program.cs
+++ service/Program.cs
@@ -6,7 +6,6 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddHttpClient("Sonnen", httpClient =>
         {
             var url = context.Configuration["Sonnen:BaseUrl"]?.ToString();
-            var apiKey = context.Configuration["Sonnen:ApiKey"]?.ToString();
             httpClient.BaseAddress = new Uri($"{url}api/v2/");
         });
         services.AddHttpClient("ChargeHq", httpClient =>
