473339a8106ddc6a63115f2f8aa8c9e2970f25b2
lucasheight <lyndengarrett@gmail.com>
2023-08-29 17:34:22 +1000

    cleanup

--- goService/service.go
+++ goService/service.go
@@ -3,16 +3,12 @@ package main
 import (
 	"bytes"
 	"fmt"
-
-	//"crypto/subtle"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
-
-	//"github.com/cenkalti/backoff/v4"
 	"github.com/joho/godotenv"
 )
 
@@ -32,7 +28,6 @@ func init() {
 }
 func main() {
 	info.Printf("%s has started \n", ServiceName)
-
 	poll()
 }
 func poll() {
@@ -40,38 +35,28 @@ func poll() {
 	if err != nil {
 		ms = ChargeHqDefaultRefreshMs
 	}
-
 	worker()
 	interval := time.Tick(time.Duration(ms) * time.Millisecond)
 	for range interval {
 		worker()
 	}
 }
-
 func worker() {
 	read := make(chan SonnenStatus)
 	go get(read, "/api/v2/status/")
-
 	select {
 	case res := <-read:
 		if res.StatusCode != 200 {
 			warn.Printf("GET-%d: %+v \n", res.StatusCode, res.Status)
-
 		} else {
 			info.Printf("Sonnen data read: %+v \n", res)
 			//now post it
 			go post(res)
 		}
-
 	case <-time.After(time.Second * 5):
 		info.Println("GET timeout received.")
-
-		// default:
-		// 	info.Println("Nothing here - bad?")
 	}
-
 }
-
 func post(data SonnenStatus) {
 	mapped := func() ChargeHq {
 		ch := ChargeHq{}
@@ -85,16 +70,17 @@ func post(data SonnenStatus) {
 		return ch
 	}()
 	charge := map[string]interface{}{
-		"apiKey": mapped.apiKey}
-
+		"apiKey": mapped.apiKey
+	}
 	meters := map[string]interface{}{
 		"consumption_kw":       mapped.siteMeters.consumption_kw,
 		"production_kw":        mapped.siteMeters.production_kw,
 		"net_import_kw":        mapped.siteMeters.net_import_kw,
 		"battery_soc":          mapped.siteMeters.battery_soc,
-		"battery_discharge_kw": mapped.siteMeters.battery_discharge_kw}
-	charge["siteMeters"] = meters
+		"battery_discharge_kw": mapped.siteMeters.battery_discharge_kw
+	}
 
+	charge["siteMeters"] = meters
 	var postBuffer bytes.Buffer
 	er := json.NewEncoder(&postBuffer).Encode(charge)
 	if er != nil {
@@ -108,36 +94,29 @@ func post(data SonnenStatus) {
 		warn.Printf("POST-Endpoint: %s \n", er.Error())
 		return
 	}
-
 	if resp.StatusCode != 200 {
 		warn.Printf("POST-%d %s. : %s \n", resp.StatusCode, resp.Status, endpointUrl)
 		return
 	}
 	defer resp.Body.Close()
 	info.Printf("ChargeHq data sent: %+v \n", mapped)
-
 }
 func get(read chan<- SonnenStatus, url string) {
 	sonnenClient := http.Client{}
 	var req *http.Response
 	var err error
-	var theUrl = getEnv(SonnenBaseUrl) + url // "/api/v2/statusf/"
+	var theUrl = getEnv(SonnenBaseUrl) + url 
 	req, err = sonnenClient.Get(theUrl)
-	//err = errors.New("Really bad error")
-	//time.Sleep(time.Second * 2)
 	sonnenData := SonnenStatus{}
 	if err != nil {
 		sonnenData.StatusCode = 500
 		sonnenData.Status = fmt.Sprintf("Server Error: %s \n", err.Error())
 		read <- sonnenData
 		return
-
 	}
 	sonnenData.StatusCode = req.StatusCode
 	sonnenData.Status = req.Status
-
 	defer req.Body.Close()
-
 	dec := json.NewDecoder(req.Body)
 	decError := dec.Decode(&sonnenData)
 	if decError != nil {
@@ -158,6 +137,5 @@ func getEnv(key string) string {
 	if !hasVal {
 		defer err.Fatal(MissingEnv)
 	}
-
 	return val
 }
